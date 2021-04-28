38004010ac6c67f2a91843385bebb2deb18c94c2
Simone Odegard <Simone.Odegard@gmail.com>
2021-04-27 20:49:03 -0700

    completed assignment

--- src/auth/middleware/bearer.js
+++ src/auth/middleware/bearer.js
@@ -6,21 +6,15 @@ module.exports = async (req, res, next) => {
 
   if (!req.headers.authorization) { next('Invalid Login') }
 
-  let token = req.headers.authorization.split(' ').pop();
-  
-  users.authenticateWithToken(token)
-    .then(validUser => {
-      req.user = validUser;
-      next();
-    })
-    .catch(e => res.status(403).send('Invalid Login'));
+  try {
+    let token = req.headers.authorization.split(' ').pop();
+    const validUser = await users.authenticateWithToken(token);
 
-  // const validUser = await users.authenticateWithToken(token);
-
-  // req.user = validUser;
-  // req.token = validUser.token;
-
-  // catch (e) {
-  //   res.status(403).send('Invalid Login');;
-  // }
+    req.user = validUser;
+    req.token = validUser.token;
+    next();
+  }  catch (e) {
+    res.status(403).send('Invalid Login');;
+  }
+ 
 }
